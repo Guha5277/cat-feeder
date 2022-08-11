987c1cdaaad34f83152faf7a43b2aa1851e47cef
Alexey <guha5277@gmail.com>
2022-08-11 13:30:13 +0300

    Исправил контроллер

--- src/main/java/ru/guhar4k/catfeeder/controller/HelloPi4jController.java
+++ src/main/java/ru/guhar4k/catfeeder/controller/HelloPi4jController.java
@@ -3,12 +3,14 @@ package ru.guhar4k.catfeeder.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HelloPi4jController {
 
-    @GetMapping("/hello")
+    @GetMapping
     public ResponseEntity<String> helloWorld() {
         return new ResponseEntity<>("Hello there!", HttpStatus.OK);
     }
